0a90b320d31c9f7bf440005ed4eb663bbaa6c891
Kazuhisa Hara <khara@sios.com>
2019-04-17 14:13:54 +0900

    misc
    Signed-off-by: Kazuhisa Hara <khara@sios.com>

--- server.go
+++ server.go
@@ -8,19 +8,15 @@ import (
 )
 
 type String string
-type exitHandler string
 
 func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, s)
-
 	log.Println("Exit 2")
 	os.Exit(2)
-
 }
 
 func main() {
 	http.Handle("/", String("Hello World."))
-
 	log.Println("Server is running at 8080 port.")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
